a7eab8f5496544464491194d61edd62111e45f8f
theinternauts <cricketw@WeAreTheInternauts.com>
2022-11-19 23:45:03 +0900

    restore deleted files FOR NOW

---
+++ SettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.h
+//  BananaCamera
+//
+//  Created by Isaac Ruiz on 9/28/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+@property (nonatomic,strong) IBOutlet UIView *footerView;
+@end
